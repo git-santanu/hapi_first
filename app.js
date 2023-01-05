557718038dbba8fe14c6b72497699eb074b47889
git-santanu <sddutta90@gmail.com>
2023-01-05 20:28:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+'use strict';
+require('dotenv').config();
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+const server = require('./config/server');
+const baseRouter = require('./routes');
+const Pack = require('./package');
+const init = async () => {
+
+    const swaggerOptions = {
+        info:{
+            title:'Test API Documentation',
+            version:Pack.version,
+        },
+        schemes: ['http','https']
+    }
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request,h)=>{
+            return 'Hello Hapi JS'
+        }
+    })
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin:HapiSwagger,
+            options:swaggerOptions
+        }
+    ])
+
+    await server.register(baseRouter,{
+		routes:{
+			prefix:'/api'
+		}
+	});
+
+    server.events.on('response', function (request) {
+        console.log(request.info.remoteAddress + ': ' + request.method.toUpperCase() + ' ' + request.path + ' --> ' + request.response.statusCode);
+    });
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
